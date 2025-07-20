acca2f65ff80c76d407038beb6454adade318c09
ankush-rana-03 <ankushrana830@gmail.com>
2025-07-20 21:19:49 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MyNavbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import FeaturesSection from "./components/FeaturesSection";
 import Footer from "./components/Footer";
 import AboutUs from "./components/AboutUs";
 import Login from "./components/Login";
-import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <MyNavbar />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <HeroSection />
-              <FeaturesSection />
-              <AboutUs />
-            </>
-          }
-        />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-      <Footer />
-    </>
-  );
-}
+      <Router>
+            <MyNavbar />
+                  <Routes>
+                          <Route
+                                    path="/"
+                                              element={
+                                                          <>
+                                                                        <HeroSection />
+                                                                                      <FeaturesSection />
+                                                                                                    <AboutUs />
+                                                                                                                  <Footer />
+                                                                                                                              </>
+                                                                                                                                        }
+                                                                                                                                                />
+                                                                                                                                                        <Route path="/login" element={<Login />} />
+                                                                                                                                                              </Routes>
+                                                                                                                                                                  </Router>
+                                                                                                                                                                    );
+                                                                                                                                                                    }
 
-export default App;
+                                                                                                                                                                    export default App;
